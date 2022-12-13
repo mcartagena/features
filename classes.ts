231fa65dbb2d7e5b144d027cd259518221781fa0
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2022-12-13 13:26:22 -0300

    Defining interfaces and classes

---
+++ classes.ts
@@ -0,0 +1,41 @@
+class Vehicle {
+
+  /* 
+  color: string;
+
+  constructor(color: string){
+      this.color = color;
+  } 
+  */
+
+  // shortcut for the above definition
+  constructor(public color: string){
+
+  }
+
+  protected honk(): void {
+    console.log('beep');
+  }
+}
+
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
+class Car extends Vehicle{
+
+  constructor(public wheels: number, color: string){
+    super(color);
+  }
+
+  private drive(): void { 
+      console.log('vroom');  
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car( 4, 'red');
+
+car.startDrivingProcess();
